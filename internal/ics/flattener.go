9ad22efcebeac4b6044a88dc3022988c79ac7b98
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-02-12 23:39:56 +0800

    Add ICS writer with VEVENT/VTODO/VJOURNAL export and RRULE flattener with EXDATE/RDATE support

---
+++ internal/ics/flattener.go
@@ -0,0 +1,129 @@
+package ics
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gongahkia/calendar-converter/internal/model"
+)
+
+type Flattener struct{}
+
+func NewFlattener() *Flattener {
+	return &Flattener{}
+}
+
+func (f *Flattener) FlattenRecurrence(item *model.CalendarItem, maxOccurrences int, endDate time.Time) []model.CalendarItem {
+	if item.Recurrence == nil {
+		return []model.CalendarItem{*item}
+	}
+
+	fmt.Fprintf(os.Stderr, "Warning: Flattening recurring item '%s' - target app lacks RRULE support\n", item.Title)
+
+	instances := []model.CalendarItem{}
+	rec := item.Recurrence
+	
+	current := item.StartTime
+	if current == nil && item.DueDate != nil {
+		current = item.DueDate
+	}
+	if current == nil {
+		return []model.CalendarItem{*item}
+	}
+
+	count := 0
+	maxCount := maxOccurrences
+	if rec.Count != nil && *rec.Count < maxCount {
+		maxCount = *rec.Count
+	}
+
+	until := endDate
+	if rec.Until != nil && rec.Until.Before(until) {
+		until = *rec.Until
+	}
+
+	for count < maxCount {
+		instance := *item
+		instance.Recurrence = nil
+		
+		if isExDate(*current, rec.ExDates) {
+			current = f.advance(current, rec)
+			continue
+		}
+
+		if item.StartTime != nil {
+			newStart := *current
+			instance.StartTime = &newStart
+		}
+		if item.DueDate != nil {
+			newDue := *current
+			instance.DueDate = &newDue
+		}
+		if item.EndTime != nil && item.StartTime != nil {
+			duration := item.EndTime.Sub(*item.StartTime)
+			newEnd := current.Add(duration)
+			instance.EndTime = &newEnd
+		}
+
+		instance.UID = fmt.Sprintf("%s-occurrence-%d", item.UID, count)
+		instances = append(instances, instance)
+		count++
+
+		current = f.advance(current, rec)
+		if current.After(until) {
+			break
+		}
+	}
+
+	for _, rdate := range rec.RDates {
+		if !isExDate(rdate, rec.ExDates) && rdate.Before(until) {
+			instance := *item
+			instance.Recurrence = nil
+			if item.StartTime != nil {
+				instance.StartTime = &rdate
+			}
+			if item.DueDate != nil {
+				instance.DueDate = &rdate
+			}
+			instance.UID = fmt.Sprintf("%s-rdate-%d", item.UID, rdate.Unix())
+			instances = append(instances, instance)
+		}
+	}
+
+	return instances
+}
+
+func (f *Flattener) advance(current *time.Time, rec *model.Recurrence) *time.Time {
+	if current == nil {
+		return nil
+	}
+
+	next := *current
+	interval := rec.Interval
+	if interval == 0 {
+		interval = 1
+	}
+
+	switch rec.Freq {
+	case model.FreqDaily:
+		next = next.AddDate(0, 0, interval)
+	case model.FreqWeekly:
+		next = next.AddDate(0, 0, 7*interval)
+	case model.FreqMonthly:
+		next = next.AddDate(0, interval, 0)
+	case model.FreqYearly:
+		next = next.AddDate(interval, 0, 0)
+	}
+
+	return &next
+}
+
+func isExDate(t time.Time, exdates []time.Time) bool {
+	for _, ex := range exdates {
+		if t.Year() == ex.Year() && t.Month() == ex.Month() && t.Day() == ex.Day() {
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/ics/writer.go
@@ -0,0 +1,362 @@
+package ics
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/emersion/go-ical"
+	"github.com/gongahkia/calendar-converter/internal/model"
+)
+
+type Writer struct{}
+
+func NewWriter() *Writer {
+	return &Writer{}
+}
+
+func (w *Writer) WriteFile(collection *model.CalendarCollection, filePath string) error {
+	f, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create ICS file: %w", err)
+	}
+	defer f.Close()
+
+	return w.Write(collection, f)
+}
+
+func (w *Writer) Write(collection *model.CalendarCollection, writer io.Writer) error {
+	cal := ical.NewCalendar()
+	cal.Props.SetText(ical.PropVersion, "2.0")
+	cal.Props.SetText(ical.PropProductID, "-//gongahkia//calendar-converter//EN")
+
+	for _, item := range collection.Items {
+		var comp *ical.Component
+		
+		switch item.ItemType {
+		case model.ItemTypeEvent:
+			comp = w.writeEvent(&item)
+		case model.ItemTypeTask:
+			comp = w.writeTodo(&item)
+		case model.ItemTypeJournal:
+			comp = w.writeJournal(&item)
+		default:
+			continue
+		}
+		
+		if comp != nil {
+			cal.Children = append(cal.Children, comp)
+		}
+	}
+
+	enc := ical.NewEncoder(writer)
+	return enc.Encode(cal)
+}
+
+func (w *Writer) writeEvent(item *model.CalendarItem) *ical.Component {
+	event := ical.NewComponent(ical.CompEvent)
+	
+	event.Props.SetText(ical.PropUID, item.UID)
+	event.Props.SetText(ical.PropSummary, item.Title)
+	
+	if item.Description != "" {
+		event.Props.SetText(ical.PropDescription, item.Description)
+	}
+	
+	if item.Location != "" {
+		event.Props.SetText(ical.PropLocation, item.Location)
+	}
+	
+	if item.StartTime != nil {
+		w.setDateTime(event.Props, ical.PropDateTimeStart, *item.StartTime, item.IsAllDay, item.Timezone)
+	}
+	
+	if item.EndTime != nil {
+		w.setDateTime(event.Props, ical.PropDateTimeEnd, *item.EndTime, item.IsAllDay, item.Timezone)
+	}
+	
+	if item.Priority > 0 {
+		event.Props.SetText(ical.PropPriority, fmt.Sprintf("%d", exportPriority(item.Priority)))
+	}
+	
+	if len(item.Tags) > 0 {
+		event.Props.SetText(ical.PropCategories, strings.Join(item.Tags, ","))
+	}
+	
+	if item.Recurrence != nil {
+		rrule := w.formatRRule(item.Recurrence)
+		event.Props.SetText(ical.PropRRule, rrule)
+		
+		if len(item.Recurrence.ExDates) > 0 {
+			w.setExDates(event.Props, item.Recurrence.ExDates, item.Timezone)
+		}
+		
+		if len(item.Recurrence.RDates) > 0 {
+			w.setRDates(event.Props, item.Recurrence.RDates, item.Timezone)
+		}
+	}
+	
+	for _, reminder := range item.Reminders {
+		alarm := w.writeAlarm(&reminder)
+		if alarm != nil {
+			event.Children = append(event.Children, alarm)
+		}
+	}
+	
+	return event
+}
+
+func (w *Writer) writeTodo(item *model.CalendarItem) *ical.Component {
+	todo := ical.NewComponent(ical.CompToDo)
+	
+	todo.Props.SetText(ical.PropUID, item.UID)
+	todo.Props.SetText(ical.PropSummary, item.Title)
+	
+	if item.Description != "" {
+		todo.Props.SetText(ical.PropDescription, item.Description)
+	}
+	
+	if item.DueDate != nil {
+		w.setDateTime(todo.Props, ical.PropDue, *item.DueDate, false, item.Timezone)
+	}
+	
+	if item.StartTime != nil {
+		w.setDateTime(todo.Props, ical.PropDateTimeStart, *item.StartTime, false, item.Timezone)
+	}
+	
+	if item.Status != model.StatusPending {
+		todo.Props.SetText(ical.PropStatus, exportStatus(item.Status))
+	}
+	
+	if item.CompletionDate != nil {
+		w.setDateTime(todo.Props, ical.PropCompleted, *item.CompletionDate, false, item.Timezone)
+		todo.Props.SetText(ical.PropPercentComplete, "100")
+	} else if item.Status == model.StatusCompleted {
+		todo.Props.SetText(ical.PropPercentComplete, "100")
+	}
+	
+	if item.Priority > 0 {
+		todo.Props.SetText(ical.PropPriority, fmt.Sprintf("%d", exportPriority(item.Priority)))
+	}
+	
+	if len(item.Tags) > 0 {
+		todo.Props.SetText(ical.PropCategories, strings.Join(item.Tags, ","))
+	}
+	
+	if item.Recurrence != nil {
+		rrule := w.formatRRule(item.Recurrence)
+		todo.Props.SetText(ical.PropRRule, rrule)
+	}
+	
+	for _, reminder := range item.Reminders {
+		alarm := w.writeAlarm(&reminder)
+		if alarm != nil {
+			todo.Children = append(todo.Children, alarm)
+		}
+	}
+	
+	return todo
+}
+
+func (w *Writer) writeJournal(item *model.CalendarItem) *ical.Component {
+	journal := ical.NewComponent(ical.CompJournal)
+	
+	journal.Props.SetText(ical.PropUID, item.UID)
+	journal.Props.SetText(ical.PropSummary, item.Title)
+	
+	if item.Description != "" {
+		journal.Props.SetText(ical.PropDescription, item.Description)
+	}
+	
+	if item.StartTime != nil {
+		w.setDateTime(journal.Props, ical.PropDateTimeStart, *item.StartTime, false, item.Timezone)
+	}
+	
+	return journal
+}
+
+func (w *Writer) writeAlarm(reminder *model.Reminder) *ical.Component {
+	alarm := ical.NewComponent(ical.CompAlarm)
+	alarm.Props.SetText(ical.PropAction, "DISPLAY")
+	alarm.Props.SetText(ical.PropDescription, "Reminder")
+	
+	if reminder.Offset != nil {
+		alarm.Props.SetText(ical.PropTrigger, formatDuration(*reminder.Offset))
+	} else if reminder.AbsoluteTime != nil {
+		alarm.Props.SetDateTime(ical.PropTrigger, *reminder.AbsoluteTime)
+	} else {
+		return nil
+	}
+	
+	return alarm
+}
+
+func (w *Writer) setDateTime(props *ical.Props, propName string, t time.Time, isAllDay bool, tz string) {
+	if isAllDay {
+		props.SetText(propName, t.Format("20060102"))
+		props.Get(propName).Params[ical.ParamValue] = []string{"DATE"}
+	} else if tz != "" && tz != "UTC" {
+		props.SetText(propName, t.Format("20060102T150405"))
+		props.Get(propName).Params[ical.ParamTimezoneID] = []string{tz}
+	} else {
+		props.SetText(propName, t.UTC().Format("20060102T150405Z"))
+	}
+}
+
+func (w *Writer) setExDates(props *ical.Props, dates []time.Time, tz string) {
+	var formatted []string
+	for _, d := range dates {
+		if tz != "" && tz != "UTC" {
+			formatted = append(formatted, d.Format("20060102T150405"))
+		} else {
+			formatted = append(formatted, d.UTC().Format("20060102T150405Z"))
+		}
+	}
+	props.SetText(ical.PropExDate, strings.Join(formatted, ","))
+	if tz != "" && tz != "UTC" {
+		props.Get(ical.PropExDate).Params[ical.ParamTimezoneID] = []string{tz}
+	}
+}
+
+func (w *Writer) setRDates(props *ical.Props, dates []time.Time, tz string) {
+	var formatted []string
+	for _, d := range dates {
+		if tz != "" && tz != "UTC" {
+			formatted = append(formatted, d.Format("20060102T150405"))
+		} else {
+			formatted = append(formatted, d.UTC().Format("20060102T150405Z"))
+		}
+	}
+	props.SetText(ical.PropRDate, strings.Join(formatted, ","))
+	if tz != "" && tz != "UTC" {
+		props.Get(ical.PropRDate).Params[ical.ParamTimezoneID] = []string{tz}
+	}
+}
+
+func (w *Writer) formatRRule(rec *model.Recurrence) string {
+	var parts []string
+	
+	parts = append(parts, fmt.Sprintf("FREQ=%s", rec.Freq))
+	
+	if rec.Interval > 1 {
+		parts = append(parts, fmt.Sprintf("INTERVAL=%d", rec.Interval))
+	}
+	
+	if rec.Count != nil {
+		parts = append(parts, fmt.Sprintf("COUNT=%d", *rec.Count))
+	}
+	
+	if rec.Until != nil {
+		parts = append(parts, fmt.Sprintf("UNTIL=%s", rec.Until.UTC().Format("20060102T150405Z")))
+	}
+	
+	if len(rec.ByDay) > 0 {
+		var days []string
+		for _, day := range rec.ByDay {
+			days = append(days, string(day))
+		}
+		parts = append(parts, fmt.Sprintf("BYDAY=%s", strings.Join(days, ",")))
+	}
+	
+	if len(rec.ByMonth) > 0 {
+		var months []string
+		for _, month := range rec.ByMonth {
+			months = append(months, fmt.Sprintf("%d", month))
+		}
+		parts = append(parts, fmt.Sprintf("BYMONTH=%s", strings.Join(months, ",")))
+	}
+	
+	if len(rec.ByMonthDay) > 0 {
+		var days []string
+		for _, day := range rec.ByMonthDay {
+			days = append(days, fmt.Sprintf("%d", day))
+		}
+		parts = append(parts, fmt.Sprintf("BYMONTHDAY=%s", strings.Join(days, ",")))
+	}
+	
+	if len(rec.BySetPos) > 0 {
+		var positions []string
+		for _, pos := range rec.BySetPos {
+			positions = append(positions, fmt.Sprintf("%d", pos))
+		}
+		parts = append(parts, fmt.Sprintf("BYSETPOS=%s", strings.Join(positions, ",")))
+	}
+	
+	return strings.Join(parts, ";")
+}
+
+func formatDuration(d time.Duration) string {
+	isNegative := d < 0
+	if isNegative {
+		d = -d
+	}
+	
+	hours := int(d.Hours())
+	minutes := int(d.Minutes()) % 60
+	seconds := int(d.Seconds()) % 60
+	
+	days := hours / 24
+	hours = hours % 24
+	
+	var parts []string
+	if isNegative {
+		parts = append(parts, "-P")
+	} else {
+		parts = append(parts, "P")
+	}
+	
+	if days > 0 {
+		parts = append(parts, fmt.Sprintf("%dD", days))
+	}
+	
+	if hours > 0 || minutes > 0 || seconds > 0 {
+		parts = append(parts, "T")
+		if hours > 0 {
+			parts = append(parts, fmt.Sprintf("%dH", hours))
+		}
+		if minutes > 0 {
+			parts = append(parts, fmt.Sprintf("%dM", minutes))
+		}
+		if seconds > 0 {
+			parts = append(parts, fmt.Sprintf("%dS", seconds))
+		}
+	}
+	
+	if len(parts) == 1 {
+		return "PT0S"
+	}
+	
+	return strings.Join(parts, "")
+}
+
+func exportPriority(p model.Priority) int {
+	switch p {
+	case model.PriorityHighest:
+		return 1
+	case model.PriorityHigh:
+		return 3
+	case model.PriorityMedium:
+		return 5
+	case model.PriorityLow:
+		return 7
+	case model.PriorityLowest:
+		return 9
+	default:
+		return 0
+	}
+}
+
+func exportStatus(s model.Status) string {
+	switch s {
+	case model.StatusCompleted:
+		return "COMPLETED"
+	case model.StatusInProgress:
+		return "IN-PROCESS"
+	case model.StatusCancelled:
+		return "CANCELLED"
+	default:
+		return "NEEDS-ACTION"
+	}
+}
