5b33cd4c8a78c31727ed52250957317210d9b19a
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-02-12 23:54:13 +0800

    Add OmniFocus TaskPaper parser/writer with tag annotations

---
+++ internal/parsers/omnifocus.go
@@ -0,0 +1,128 @@
+package parsers
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/gongahkia/calendar-converter/internal/model"
+)
+
+type OmniFocusParser struct{}
+
+func NewOmniFocusParser() *OmniFocusParser {
+	return &OmniFocusParser{}
+}
+
+func (p *OmniFocusParser) ParseFile(filePath string) (*model.CalendarCollection, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return p.Parse(f, filePath)
+}
+
+func (p *OmniFocusParser) Parse(r io.Reader, sourcePath string) (*model.CalendarCollection, error) {
+	collection := &model.CalendarCollection{
+		Items:            []model.CalendarItem{},
+		SourceApp:        "omnifocus",
+		ExportDate:       time.Now(),
+		OriginalFilePath: sourcePath,
+	}
+
+	scanner := bufio.NewScanner(r)
+	var currentItem *model.CalendarItem
+	var indent int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		
+		currentIndent := 0
+		for strings.HasPrefix(line, "\t") {
+			currentIndent++
+			line = strings.TrimPrefix(line, "\t")
+		}
+
+		if strings.HasPrefix(line, "- ") {
+			if currentItem != nil {
+				collection.Items = append(collection.Items, *currentItem)
+			}
+
+			line = strings.TrimPrefix(line, "- ")
+			item := model.CalendarItem{
+				ItemType: model.ItemTypeTask,
+				Status:   model.StatusPending,
+			}
+
+			parts := strings.SplitN(line, " @", 2)
+			item.Title = strings.TrimSpace(parts[0])
+
+			if len(parts) > 1 {
+				parseOmniFocusTags(&item, "@"+parts[1])
+			}
+
+			currentItem = &item
+			indent = currentIndent
+		} else if currentItem != nil && strings.TrimSpace(line) != "" {
+			currentItem.Description += strings.TrimSpace(line) + "\n"
+		}
+	}
+
+	if currentItem != nil {
+		collection.Items = append(collection.Items, *currentItem)
+	}
+
+	return collection, nil
+}
+
+func parseOmniFocusTags(item *model.CalendarItem, tagStr string) {
+	tagRe := regexp.MustCompile(`@(\w+)(?:\(([^)]+)\))?`)
+	matches := tagRe.FindAllStringSubmatch(tagStr, -1)
+
+	for _, match := range matches {
+		tagName := match[1]
+		tagValue := ""
+		if len(match) > 2 {
+			tagValue = match[2]
+		}
+
+		switch strings.ToLower(tagName) {
+		case "due":
+			if t, err := parseOmniFocusDate(tagValue); err == nil {
+				item.DueDate = &t
+			}
+		case "defer":
+			if t, err := parseOmniFocusDate(tagValue); err == nil {
+				item.StartTime = &t
+			}
+		case "done":
+			item.Status = model.StatusCompleted
+		case "tags":
+			item.Tags = append(item.Tags, strings.Split(tagValue, ",")...)
+		default:
+			item.Tags = append(item.Tags, tagName)
+		}
+	}
+}
+
+func parseOmniFocusDate(dateStr string) (time.Time, error) {
+	formats := []string{
+		"2006-01-02",
+		"2006-01-02 15:04",
+		"Jan 2, 2006",
+		"January 2, 2006",
+	}
+
+	for _, format := range formats {
+		if t, err := time.Parse(format, dateStr); err == nil {
+			return t, nil
+		}
+	}
+
+	return time.Time{}, fmt.Errorf("unable to parse date: %s", dateStr)
+}
---
+++ internal/writers/omnifocus.go
@@ -0,0 +1,76 @@
+package writers
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/gongahkia/calendar-converter/internal/model"
+)
+
+type OmniFocusWriter struct{}
+
+func NewOmniFocusWriter() *OmniFocusWriter {
+	return &OmniFocusWriter{}
+}
+
+func (w *OmniFocusWriter) WriteFile(collection *model.CalendarCollection, filePath string) error {
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return w.Write(collection, f)
+}
+
+func (w *OmniFocusWriter) Write(collection *model.CalendarCollection, writer io.Writer) error {
+	for _, item := range collection.Items {
+		line := "- " + item.Title
+
+		if item.DueDate != nil {
+			line += fmt.Sprintf(" @due(%s)", item.DueDate.Format("2006-01-02"))
+		}
+
+		if item.StartTime != nil {
+			line += fmt.Sprintf(" @defer(%s)", item.StartTime.Format("2006-01-02"))
+		}
+
+		if len(item.Tags) > 0 {
+			line += " @tags(" + strings.Join(item.Tags, ",") + ")"
+		}
+
+		if item.Status == model.StatusCompleted {
+			line += " @done"
+		}
+
+		if _, err := fmt.Fprintln(writer, line); err != nil {
+			return err
+		}
+
+		if item.Description != "" {
+			descLines := strings.Split(strings.TrimSpace(item.Description), "\n")
+			for _, descLine := range descLines {
+				if _, err := fmt.Fprintln(writer, "\t"+descLine); err != nil {
+					return err
+				}
+			}
+		}
+
+		for _, subtask := range item.Subtasks {
+			subLine := "\t- " + subtask.Title
+			if subtask.Status == model.StatusCompleted {
+				subLine += " @done"
+			}
+			if _, err := fmt.Fprintln(writer, subLine); err != nil {
+				return err
+			}
+		}
+
+		if _, err := fmt.Fprintln(writer); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
